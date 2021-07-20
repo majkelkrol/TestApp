ec8fa3b1b9d8553868fab6199a25c1030e1d33fe
Majkel <majkelkrol@icloud.com>
2021-07-20 11:31:16 +0200

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Majkel on 20/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
